2773895e849e6656fb4c511107d2dc525c61a9b2
SnoorSalihi <eng.sinoor@gmail.com>
2024-05-08 22:58:31 -0400

    update

--- Script.js
+++ Script.js
@@ -3,12 +3,25 @@ const count = 8;
 const rows = [];
 
 function padRow(rowNumber, rowCount) {
-  return character.repeat(rowNumber);
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
-for (let i = 0; i < count; i = i + 1) {
-    rows.push(padRow())
+
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
+
+let continueLoop = false;
+let done = 0;
+
+while (continueLoop) {
+  done++;
+if (done === count) {
+
   }
-  let result = ""
+}
+
+let result = ""
 
 for (const row of rows) {
   result = result + "\n" + row;
